679f8950c1704ef7454493d82e3618d7c764065a
Ruslan Shafikov <rs31113@yandex.ru>
2025-12-26 23:41:30 +0300

    feat: implement notes CRUD endpoints (NRA-17)

--- src/notes/views.py
+++ src/notes/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+import notes.models
+import notes.serializers
+
+
+class NoteViewSet(viewsets.ModelViewSet):
+    serializer_class = notes.serializers.NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return notes.models.Note.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
