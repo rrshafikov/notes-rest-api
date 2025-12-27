43e3449a17b926b81d0edbfc23907bc5d0588e47
Nikita Belov <nikitabelov724@gmail.com>
2025-12-27 12:10:57 +0300

    chore: add OpenAPI schema and Swagger UI (NRA-21)

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
+import drf_spectacular.views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    # OpenAPI schema + Swagger UI
+    path("api/schema/", drf_spectacular.views.SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", drf_spectacular.views.SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+
+    # API
     path("api/", include("notes.urls")),
 ]
